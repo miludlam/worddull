a17f6176d08b86a59246175c37ace4cf63810973
miludlam <miludlam@gmail.com>
2024-06-17 20:34:14 -0400

    Initial JS skeleton and ID game tiles

--- word_master.js
+++ word_master.js
@@ -1,4 +1,39 @@
+const letters = document.querySelectorAll('.tile');
+const loading = document.querySelector('.info-bar');
+
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
 
+function submitGuess() {
+    console.log("Submitting...");
+}
+
+function updateTile() {
+    console.log("Updating...");
+}
+
+async function init() {
+
+    document.addEventListener('keydown', function handleKeyPress (event) {
+        const action = event.key;
+
+        switch (action) {
+            case 'Enter':
+                submitGuess();
+                break;
+            case 'Backspace':
+                updateTile();
+                break;
+            default:
+                if (!isLetter(action)) {
+                    event.preventDefault();
+                } else {
+                    updateTile();
+                }
+                break;
+        }
+    });
+}
+
+init();
