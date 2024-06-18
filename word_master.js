7ac2a9c8f597fc37aff48593feb0006ad3254d68
miludlam <115509062+miludlam@users.noreply.github.com>
2024-06-18 13:31:46 -0400

    Update word_master.js to handle keypress and backspace scenarios

--- word_master.js
+++ word_master.js
@@ -1,9 +1,13 @@
 const letters = document.querySelectorAll('.tile');
 const loading = document.querySelector('.info-bar');
+const firstRow = 0;
+const lastRow = 5;
+const firstCol = 0;
+const lastCol = 4;
 const wordle = '';
 
-// Index translates to [row, column]
-let currentIdx = [0, 0];
+// Indices translate to [row, column]
+let currentIdx = [firstRow, firstCol];
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
@@ -12,51 +16,66 @@ function isLetter(letter) {
 function submitGuess() {
     console.log("Submitting...");
     
-    // 1. handle invalid
-    // 2. check against word of the day and
+    // 1. handle invalid word
+    // 2. check against word of the day and...
     // 3. win condition
     // 4. non-win condition 
 }
 
-function updateTile(key) {
-    // Handle invalid index
-    if (currentIdx[1] > 4 || currentIdx[1] < 0) {
+function clearTile(key) {
+    // Can only be at row 0 if tile is already empty
+    if (currentIdx[1] === firstCol) {
         return;
-    }
+    } 
 
-    let pos = 'row_' + currentIdx[0] + '_' + currentIdx[1];
-    console.log("Updating..." + pos);
-    
-    let tile = document.getElementById(pos);
+    let tile = getTile();
 
-    if (key === 'Backspace') {
-        if (currentIdx[1] >= 0) {
-            currentIdx[1]--;
-            tile.innerHTML = '';
-        }
+    if (currentIdx[1] === lastCol && tile.getInnerHTML().length === 1) {
+        tile.innerHTML = '';
     } else {
-        if (currentIdx[1] <= 4) {
-            tile.innerHTML = key;
-            currentIdx[1]++;
-        }
+        currentIdx[1]--;
+        tile = getTile();
+        tile.innerHTML = '';
     }
 }
 
-async function init() {
+function letterTile(key) {
+    let tile = getTile();
+    
+    if (currentIdx[1] === lastCol && tile.innerHTML.length === 1) {
+        return;
+    } else {
+        console.log(tile);
+        tile.innerHTML = key;
+        if (currentIdx[1] !== lastCol) {
+            currentIdx[1]++;
+        }
+    }
+    
+}
 
+// A simple getter to return the ID of the current tile
+function getCurrentTileID() {
+    return 'row_' + currentIdx[0] + '_' + currentIdx[1];
+}
+
+// Returns the current tile in the gameboard
+function getTile() {
+    return document.getElementById(getCurrentTileID());
+}
+
+async function init() {
     document.addEventListener('keydown', function handleKeyPress (event) {
         const key = event.key;
 
-        switch (key) {
-            case 'Enter':
-                submitGuess();
-                break;
-            case 'Backspace':
-                updateTile(key);
-                break;
-            default:
-                updateTile(key.toUpperCase());
-                break;
+        if (key === 'Enter') {
+            submitGuess();
+        } else if (key === 'Backspace') {
+            clearTile(key);
+        } else if (isLetter(key)) {
+            letterTile(key.toUpperCase());
+        } else {
+            event.preventDefault();
         }
     });
 }
