46dd8c0e8c28e82c04dae5e5b169f381f172276d
miludlam <miludlam@gmail.com>
2024-06-17 21:25:44 -0400

    Adding letter and backspace key handling

--- word_master.js
+++ word_master.js
@@ -1,5 +1,9 @@
 const letters = document.querySelectorAll('.tile');
 const loading = document.querySelector('.info-bar');
+const wordle = '';
+
+// Index translates to [row, column]
+let currentIdx = [0, 0];
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
@@ -7,29 +11,53 @@ function isLetter(letter) {
 
 function submitGuess() {
     console.log("Submitting...");
+    
+    // 1. handle invalid
+    // 2. check against word of the day and
+    // 3. win condition
+    // 4. non-win condition 
 }
 
-function updateTile() {
-    console.log("Updating...");
+function updateTile(key) {
+    let pos = 'row_' + currentIdx[0] + '_' + currentIdx[1];
+    console.log("Updating..." + pos);
+    
+    let tile = document.getElementById(pos)
+
+    if (key === 'Backspace') {
+        tile.innerHTML = '';
+        currentIdx[1]--;
+    } else {
+        tile.innerHTML = key;
+        currentIdx[1]++;
+    }
 }
 
 async function init() {
 
     document.addEventListener('keydown', function handleKeyPress (event) {
-        const action = event.key;
+        const key = event.key;
 
-        switch (action) {
+        switch (key) {
             case 'Enter':
-                submitGuess();
-                break;
-            case 'Backspace':
-                updateTile();
-                break;
-            default:
-                if (!isLetter(action)) {
+                if (currentIdx[1] !== 4) {
                     event.preventDefault();
                 } else {
-                    updateTile();
+                    submitGuess();
+                }
+                break;
+            case 'Backspace':
+                if (currentIdx[1] === 0) {
+                    event.preventDefault();
+                } else {
+                    updateTile(key.toUpperCase);
+                }
+                break;
+            default:
+                if (!isLetter(key) || currentIdx[1] === 4) {
+                    event.preventDefault();
+                } else {
+                    updateTile(key.toUpperCase());
                 }
                 break;
         }
