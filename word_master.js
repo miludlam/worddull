78a12d83c2eec3b0f34c3c62244238c88d4aa133
miludlam <miludlam@gmail.com>
2024-06-21 22:57:37 -0400

    API connection to retrieve and validate words

--- word_master.js
+++ word_master.js
@@ -1,47 +1,92 @@
+const dailyWordURL = "https://words.dev-apis.com/word-of-the-day";
+const wordValidatorURL = "https://words.dev-apis.com/validate-word";
+let wordle = '';
+let isValid;
+
 const tiles = document.querySelectorAll('.tile');
 const loading = document.querySelector('.info-bar');
 const firstRow = 0;
 const lastRow = 5;
 const firstCol = 0;
 const lastCol = 4;
-const wordle = 'HELLO';
 
 // Indices translate to [row, column]
 let currentIdx = [firstRow, firstCol];
 let wordBuffer = '';
 let gameOver = false;
 
+async function wordOfTheDay() {
+    // connect to API and parse response
+    const response = await fetch(dailyWordURL);
+    const result = await response.json();
+    // now that we have gained the word, hide the spinning eggplant
+    loading.classList.add("hidden");
+
+    return result.word;
+}
+
+async function validateWord(guess) {
+    // build JSON to POST
+    let data = {
+        word: guess,
+    };
+
+    // Display eggplant while we await JSON response
+    loading.classList.remove("hidden");
+
+    // connect to API to validate word
+    const response = await fetch(wordValidatorURL, {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+    const result = await response.json();
+
+    // Now that we have response, seggsy eggplant
+    loading.classList.add("hidden");
+
+    return result.validWord;
+}
+
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
 
 function submitGuess() {    
-    // 1. handle invalid word
+    // 1. handle invalid word length
     if (wordBuffer.length !== wordle.length) {
         // invalid animation
+        alert("Word too short!");
         return;
     }
-    // 2. check against word of the day and...
-    if (wordBuffer === wordle) {
-        // 3. win condition
-        console.log("You win! " + wordBuffer);
-        alert("You win!");
-        gameOver = true;
-        wordBuffer = '';
-    } else {
-        // 4. non-win condition
-        console.log("Guess incorrect: " + wordBuffer);
-        if (currentIdx[0] === lastRow) {
-            // game over
-            alert("You lose!");
-            wordBuffer = '';
-            gameOver = true;
-        } else {
-            wordBuffer = '';
-            currentIdx[0]++;
-            currentIdx[1] = 0;
+    // 2. handle invalid word
+    validateWord(wordBuffer).then((isValid) => {
+        console.log(isValid);
+        if (isValid === false) {
+            alert("Invalid word!");
+            return;
         }
-    }
+        // 3. check against word of the day and...
+        if (wordBuffer === wordle) {
+            // 4. win condition
+            console.log("You win! " + wordBuffer);
+            alert("You win!");
+            gameOver = true;
+            wordBuffer = '';
+        } else {
+            // 5. non-win condition
+            console.log("Guess incorrect: " + wordBuffer);
+            if (currentIdx[0] === lastRow) {
+                // game over
+                alert("You lose!");
+                wordBuffer = '';
+                gameOver = true;
+            } else {
+                wordBuffer = '';
+                currentIdx[0]++;
+                currentIdx[1] = 0;
+            }
+        }
+    });
 }
 
 function clearTile(key) {
@@ -87,6 +132,10 @@ function getTile() {
 }
 
 async function init() {
+    wordOfTheDay().then(function (word) {
+        wordle = word.toUpperCase();    
+    });
+
     document.addEventListener('keydown', function handleKeyPress (event) {
         if (!gameOver) {
             const key = event.key;
